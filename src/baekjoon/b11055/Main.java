9048772edbd0dca72029003118139195c4dbd33a
junirii <105265611+junirii@users.noreply.github.com>
2023-08-03 16:59:16 +0900

    0802 알고리즘 스터디 문제

---
+++ src/baekjoon/b11055/Main.java
@@ -0,0 +1,19 @@
+package baekjoon.b11055;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int max = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = i; j < N; j++) {
+			}
+		}
+	}
+}
