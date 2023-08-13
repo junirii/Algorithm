a41b4ab484c058d3f7f720ed1b43714b920920c2
junirii <105265611+junirii@users.noreply.github.com>
2023-08-14 00:41:40 +0900

    0813 알고리즘

--- src/programmers/p42577/Solution.java
+++ src/programmers/p42577/Solution.java
@@ -1,23 +1,21 @@
 package programmers.p42577;
 
+import java.util.Arrays;
+
 public class Solution {
 	public static boolean solution(String[] phone_book) {
         boolean answer = true;
-        for (int i = 0; i < phone_book.length; i++) {
-			for (int j = i+1; j < phone_book.length; j++) {
-				if(!phone_book[i].substring(0, 1).equals(phone_book[j].substring(0, 1))) continue;
-				if(phone_book[i].indexOf(phone_book[j]) == 0 || phone_book[j].indexOf(phone_book[i]) == 0) {
-					answer = false;
-					break;
-				}
-			}
+        Arrays.sort(phone_book);
+        
+        for (int i = 0; i < phone_book.length-1; i++) {
+        	if(phone_book[i+1].indexOf(phone_book[i]) == 0) answer = false;
 		}
         
         return answer;
     }
 	
 	public static void main(String[] args) {
-		boolean a = solution(new String[] {"12","123","1235","567","88"});
+		boolean a = solution(new String[] {"119", "97674223", "1195524421"});
 		System.out.println(a);
 	}
 }
