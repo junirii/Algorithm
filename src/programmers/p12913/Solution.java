d713d95777fb17be5984d09e9fbfb35132d0e3ee
junirii <105265611+junirii@users.noreply.github.com>
2023-08-20 23:17:48 +0900

    0820 알고리즘

--- src/programmers/p12913/Solution.java
+++ src/programmers/p12913/Solution.java
@@ -1,29 +1,22 @@
 package programmers.p12913;
 
+import java.util.Arrays;
+
 public class Solution {
 	static int solution(int[][] land) {
-        int answer = 0;
-        int[] visited = new int[land.length];
-        for (int i = 0; i < land.length; i++) {
-			int[] arr = land[i];
-			int max = 0;
-			for (int j = 0; j < arr.length; j++) {
-				if(arr[j] > max) {
-					if(i == 0 || visited[i-1] != j) {
-						max = arr[j];
-						visited[i] = j;
-					}
-				}
-			}
-			System.out.println("max:"+max);
-			for (int j : visited) {
-				System.out.println("visited:"+j);
-			}
-			answer += max;
+		int answer = 0;
+		for (int i = 1; i < land.length; i++) {
+			land[i][0] += Math.max(Math.max(land[i-1][1], land[i-1][2]), land[i-1][3]);
+			land[i][1] += Math.max(Math.max(land[i-1][0], land[i-1][2]), land[i-1][3]);
+			land[i][2] += Math.max(Math.max(land[i-1][1], land[i-1][0]), land[i-1][3]);
+			land[i][3] += Math.max(Math.max(land[i-1][1], land[i-1][2]), land[i-1][0]);
 		}
-
+		int[] arr = land[land.length-1];
+		Arrays.sort(arr);
+		answer = arr[arr.length-1];
         return answer;
     }
+	
 	public static void main(String[] args) {
 		System.out.println(solution(new int[][] {{1,2,3,5},{5,6,7,8},{4,3,2,1}}));
 	}
