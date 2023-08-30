ee77cdae21fcef480c9edbd8c08101f7037a7aa4
junirii <105265611+junirii@users.noreply.github.com>
2023-08-30 17:37:01 +0900

    Merge branch 'master' of https://github.com/junirii/Algorithm.git

--- src/baekjoon/b2606/Main.java
+++ src/baekjoon/b2606/Main.java
@@ -3,21 +3,21 @@ package baekjoon.b2606;
 import java.util.Scanner;
 
 public class Main {
-	private static int[] parent;
+	public static int[] parent;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int M = sc.nextInt();
 		parent = new int[N+1];
-		for (int i = 1; i < parent.length; i++) {
+		for (int i = 1; i <= N; i++) {
 			parent[i] = i;
 		}
 		for (int i = 0; i < M; i++) {
 			union(sc.nextInt(), sc.nextInt());
 		}
 		int cnt = 0;
-		for (int i = 2; i < parent.length; i++) {
-			if(parent[i] == parent[1]) cnt++;
+		for (int i = 2; i <= N; i++) {
+			if(find(i) == find(1)) cnt++;
 		}
 		System.out.println(cnt);
 	}
@@ -33,6 +33,6 @@ public class Main {
 	
 	public static int find(int a) {
 		if(parent[a] == a) return a;
-		return find(parent[a]);
+		return parent[a] = find(parent[a]);
 	}
 }
