75befeecd9bf90bc3cae9ad109a213534d6aef43
junirii <105265611+junirii@users.noreply.github.com>
2023-09-21 00:01:23 +0900

    알고리즘 스터디 7주차

---
+++ src/baekjoon/p12980/Solution.java
@@ -0,0 +1,21 @@
+package baekjoon.p12980;
+
+public class Solution {
+    public static int solution(int n) {
+        int ans = 0;
+        while(n > 0) {
+        	if(n%2 == 0) {
+        		n /= 2;
+        	}else {
+        		n -= 1;
+        		ans++;
+        	}
+        }
+        return ans;
+    }
+	
+	public static void main(String[] args) {
+		int a = solution(5000);
+		System.out.println(a);
+	}
+}
---
+++ src/baekjoon/p84512/Solution.java
@@ -0,0 +1,37 @@
+package baekjoon.p84512;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Solution {
+	public static String[] wordArr = {"A", "E", "I", "O", "U"};
+	public static List<String> list = new ArrayList<>();
+	public static StringBuilder sb;
+	
+	public static int solution(String word) {
+		int answer = 0;
+
+		dfs("");
+		Collections.sort(list);
+		answer = list.indexOf(word)+1;
+		
+        return answer;
+    }
+	
+	public static void dfs(String s) {
+		if(s.length() == 5) return;
+		
+		sb = new StringBuilder(s);
+		for (int i = 0; i < wordArr.length; i++) {
+			sb.append(wordArr[i]);
+			list.add(sb.toString());
+			dfs(sb.toString());
+			sb.delete(sb.length()-1, sb.length());
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(solution("AAAE"));
+	}
+}
