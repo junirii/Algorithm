47281ec9dac47ff060e52940f6dfe2bcf7f4ac8f
junirii <105265611+junirii@users.noreply.github.com>
2023-11-16 22:29:37 +0900

    1116 알고리즘

---
+++ src/programmers/p12909/Solution.java
@@ -0,0 +1,24 @@
+package programmers.p12909;
+
+public class Solution {
+	public static void main(String[] args) {
+		boolean a = solution("()()");
+		System.out.println(a);
+	}
+	
+	static boolean solution(String s) {
+		boolean answer = true;
+		int start = 0;
+		int end = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if(s.substring(i, i+1).equals("(")) start++;
+			else end++;
+			if(end > start) {
+				answer = false;
+				break;
+			}
+		}
+		if(start != end) answer = false;
+		return answer;
+	}
+}
